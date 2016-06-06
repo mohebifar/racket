d641c2970cb79525d3e501e85ef0ca11f397ab02
Mohamad Mohebifar <netfars@gmail.com>
2016-06-07 00:33:12 +0430

    Initial commit

---
+++ src/generators/app/index.js
@@ -0,0 +1,99 @@
+import path from 'path';
+import BaseGenerator from '../base-generator';
+
+class Generator extends BaseGenerator {
+  constructor(...args) {
+    super(...args);
+
+    this.argument('name', {type: String, required: false});
+
+    this.option('skip-install', {
+      desc: 'Do not install dependencies',
+      type: Boolean,
+      defaults: false
+    });
+  }
+
+  get initializing() {
+    return {
+      init: function () {
+        this.config.set('generatorVersion', this.rootGeneratorVersion());
+        this.filters = {};
+        this.init();
+      },
+      info: function () {
+        this.log(this.yoWelcome);
+        this.log('Creating a universal react app had never been so easy!\n');
+      }
+    };
+  }
+
+  get prompting() {
+    return {
+      redux: function () {
+        const done = this.async();
+        const prompt = [
+          {
+            type: 'list',
+            name: 'asyncActions',
+            message: 'What would you prefer for async actions?',
+            choices: ['redux-saga', 'redux-thunk']
+          }
+        ];
+
+        this.prompt(prompt, ({asyncActions}) => {
+          this.options.reduxAsyncActions = asyncActions;
+          if (asyncActions === 'redux-saga') {
+            this.filters.reduxSaga = true;
+          } else if (asyncActions === 'redux-thunk') {
+            this.filters.reduxThunk = true;
+          }
+          done();
+        });
+      },
+      style: function () {
+        const done = this.async();
+        const prompt = [
+          {
+            type: 'list',
+            name: 'styling',
+            message: 'What would you prefer for styling?',
+            choices: ['sass', 'less', 'postcss', 'css']
+          }
+        ];
+
+        this.prompt(prompt, ({styling}) => {
+          this.options.styling = styling;
+          this.filters[styling] = true;
+          done();
+        });
+      }
+    };
+  }
+
+  get writing() {
+    return {
+      generateProject: function () {
+        this.sourceRoot(path.join(__dirname, '../../templates/app'));
+        this.destinationRoot();
+        this.processDirectory('.', '.');
+      }
+    };
+  }
+
+  get install() {
+    return {
+      installDeps: function () {
+        this.installDependencies({
+          skipInstall: this.options['skip-install']
+        });
+      }
+    };
+  }
+
+  get end() {
+    return {};
+  }
+}
+
+module.exports = Generator;
---
+++ src/generators/base-generator.js
@@ -0,0 +1,94 @@
+import path from 'path';
+import glob from 'glob';
+import lodash from 'lodash';
+import s from 'underscore.string';
+import fs from 'fs';
+import {Base} from 'yeoman-generator';
+
+lodash.mixin(s.exports());
+
+function expandFiles(pattern, options = {}) {
+  const cwd = options.cwd || process.cwd();
+  return glob.sync(pattern, options).filter(filePath => fs.statSync(path.join(cwd, filePath)).isFile());
+}
+
+const filterRegex = /\(([a-z0-9]+)\)/i;
+
+export default class BaseGenerator extends Base {
+  constructor(...args) {
+    super(...args);
+  }
+
+  init() {
+    const baseDetermineAppname = this.determineAppname.bind(this);
+
+    this.determineAppname = () => {
+      return this.name || baseDetermineAppname();
+    };
+
+    this.appName = lodash.camelize(
+      lodash.slugify(
+        lodash.humanize(this.determineAppname())
+      )
+    );
+
+    this.filters = this.filters || this.config.get('filters');
+  }
+
+  getTemplateParams(filePath) {
+    return {
+      lodash,
+      filePath,
+      appName: this.appName,
+      filters: this.filters
+    };
+  }
+
+  getFilteredFileName(fileName) {
+    if (filterRegex.test(fileName)) {
+      return fileName.replace(filterRegex, '');
+    } else {
+      return null;
+    }
+  }
+
+  shouldFilteredFileBeIncluded(fileName) {
+    const matches = fileName.match(filterRegex);
+    return matches && this.filters[matches[1]];
+  }
+
+  processDirectory(source, destination) {
+    const root = path.isAbsolute(source) ? source : path.join(this.sourceRoot(), source);
+    const files = expandFiles('**', {dot: true, cwd: root});
+
+    files.forEach(name => {
+      const src = path.join(root, name);
+      let ignore = false;
+      let dest = path.join(destination, name);
+      let isTemplate = false;
+
+      if (path.basename(dest).indexOf('_') === 0) {
+        const stripped = path.basename(dest).replace(/^_/, '');
+        isTemplate = true;
+        dest = path.join(path.dirname(dest), stripped);
+      }
+
+      const isFiltered = this.getFilteredFileName(path.basename(dest));
+      if (isFiltered) {
+        if (this.shouldFilteredFileBeIncluded(path.basename(dest))) {
+          dest = path.join(path.dirname(dest), isFiltered);
+        } else {
+          ignore = true;
+        }
+      }
+
+      if (!ignore) {
+        if (isTemplate) {
+          this.fs.copyTpl(src, dest, this.getTemplateParams(dest));
+        } else {
+          this.fs.copy(src, dest);
+        }
+      }
+    });
+  }
+}
