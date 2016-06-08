4df32f7319885b2e2594fdd48dd1768c1d85894d
mohebifar <netfars@gmail.com>
2016-06-08 15:55:22 +0430

    Add identifiers in reducers file

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,8 +3,10 @@ import { routerReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-connect';
 import { reducer as formReducer } from 'redux-form';
 import githubHottest from './modules/githubHottest';
+// Import reducers below
 
 export default combineReducers({
+  // Add reducers below
   githubHottest,
   reduxAsyncConnect,
   routing: routerReducer,
