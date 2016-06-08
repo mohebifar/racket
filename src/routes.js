b0e7af7cddfa616baaacace4dd07a3ebca722f4c
mohebifar <netfars@gmail.com>
2016-06-08 15:55:47 +0430

    Add identifiers in routes file

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { App, Index, GithubHottest, NotFound } from './containers';
 export default function getRoutes() {
   return (<Route path="/" component={App}>
     <IndexRoute component={Index}/>
+    { /* Add routes below */ }
     <Route path="hottest" component={GithubHottest}/>
     <Route path="*" component={NotFound} status={404}/>
   </Route>);
